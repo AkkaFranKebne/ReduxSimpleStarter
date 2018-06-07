ca0c2e439b8c08ab312916eaf44a7b76d94f3d46
AkkaFranKebne <ewawrobel@gmail.com>
2018-06-07 18:55:43 +0200

    search on input

--- src/index.js
+++ src/index.js
@@ -17,18 +17,24 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({ key: API_KEY, term: 'samurai champloo'}, (videos) => {
-      this.setState({
-        videos: videos,
-        selectedVideo: videos[0]
-      })
-    });
+    this.videoSearch('samurai champloo');
+
   }
+
+    videoSearch(term) {
+      YTSearch({ key: API_KEY, term: term }, (videos) => {
+        this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+        })
+      });
+    }
+
 
   render(){
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange = { term => this.videoSearch(term) }/>
         <VideoDetail video = { this.state.selectedVideo }/>
         <VideoList
           videos = { this.state.videos }
