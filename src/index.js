f8267744c24fa4de8f4a427117bcd74002f82d9d
AkkaFranKebne <ewawrobel@gmail.com>
2018-06-07 21:59:50 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -22,14 +22,14 @@ class App extends Component {
 
   }
 
-    videoSearch(term) {
-      YTSearch({ key: API_KEY, term: term }, (videos) => {
-        this.setState({
-          videos: videos,
-          selectedVideo: videos[0]
-        })
-      });
-    }
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
+    });
+  }
 
 
   render(){
