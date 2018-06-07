499d406cdb401dc1cf56d2e63b17ee3f5507f857
AkkaFranKebne <ewawrobel@gmail.com>
2018-06-07 12:50:00 +0200

    search bar component, onChange event

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar'
+
 const API_KEY = 'AIzaSyBNQUMP_F9N7v7sBHaGUT1UWPwzwIsihEg';
 
 const App = () => {
-  return <div>Hello World!</div>
+  return (
+    <div><SearchBar /></div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
